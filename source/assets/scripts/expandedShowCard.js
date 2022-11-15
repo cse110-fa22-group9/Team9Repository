f292a020037731a1781c733ecfc21ff383246cd4
Aedan <apbailey@ucsd.edu>
2022-11-15 12:28:27 -0800

    Co-authored-by: r3zeng <r3zeng@users.noreply.github.com> Generated seasons buttons and checkboxes depending on what season is selected. Checkboxes update to display correct episodes and booleans if a different season is selected.

--- source/assets/scripts/expandedShowCard.js
+++ source/assets/scripts/expandedShowCard.js
@@ -46,8 +46,7 @@
      *                            "showTitle" : "string"
      *                            "imgSrc" : "string"
      *                            "imgAlt" : "string"
-     *                            "numEpisodes" : number
-     *                            "episodesWatched" : number
+     *                            "episodeArray": 2D array of booleans representing show and seasons
      *                            "rating" : number
      *                            "comments" : "string"
      *                        }
@@ -55,33 +54,110 @@
     set data(data) {
         // if no data, return
         if (!data) return;
-
         const shadowDom = this.shadowRoot;
-        let article = shadowDom.querySelector('article');
+        update(data,  1, shadowDom);
 
-        // TODO: add js for a progress bar
-        // TODO: Figure out how to implement the "season" indicator, or if we're even going to do it
-
-        article.innerHTML = `   
-                            <div> 
-                                <h4 id="tvshowheader">TV Show</h4>
-                                <button> + </button>
-                                <h4>Add show</h4>
-                            </div>
-                            <img src=${data.imgSrc} alt=${data.imgAlt}>
-                            <div> 
-                                <p class="title">Name: ${data.showTitle}</p>
-                                <button>Edit </button>
-                                <button>trash </button>
-                            </div>
-                            <p class="rating">Rating: ${data.rating}/5</p>
-                            <p class="comments">Comments: ${data.comments}</p>
-                            <h2 id="progressheader">Progress: </h2>` +
-                            generateSeasonsHTML(data.episodeArray)
-                            + `<p class="watched">${WatchedEpisodes(data.episodeArray)}/${TotalEpisodes(data.episodeArray)} episodes watched</p>`;
     }
 
 }
+
+/**
+ * 
+ * @param {*} data 
+ * @param {*} seasonNumber 
+ * @param {*} shadowDom 
+ * 
+ * This function gets the article associated with the shadow and then calls innerHTML on it
+ * The generation will create the page with season buttons and checkboxes representing true or false on if the
+ * episode is watched or not
+ * 
+ * Then, createActionListeners is called to generate listerners for the buttons and checkboxes created
+ * 
+ * This function is called everytime a season button is selected
+ */
+
+function update(data, seasonNumber, shadowDom){
+    let article = shadowDom.querySelector('article');
+    article.innerHTML = generatedInnerHTML(data, seasonNumber);
+    CreateActionListeners(data, seasonNumber, shadowDom);
+}
+
+/**
+ * 
+ * @param {data passed in to expandedShowCard} data 
+ * @param {current Selected season} seasonNumber 
+ * @param {shadowDom associated with current object} shadowDom 
+ * 
+ * This function takes in the current selected season (chosen by clicking a button) and then generates event listeners to 
+ * switch seasons, if a season is clicked it will take update the current html to reflect the checkbox array for that season
+ * 
+ * For the checkboxes which are displayed depending on the season selected, event listeners are generated so that data.episodeArray
+ * booleans are updated depending on if the checkboxes are checked or unchecked
+ */
+function CreateActionListeners(data, seasonNumber, shadowDom){
+    for(let i = 0; i < data.episodeArray.length; i++){
+        let currNum = i + 1;
+        let seasonObject = shadowDom.getElementById(`season_` + (currNum) + `_button`);
+        seasonObject.addEventListener('click', () => {
+            update(data, currNum, shadowDom);
+        });
+    }
+
+    for(let i = 0; i < data.episodeArray[seasonNumber - 1].length; i++){
+        let checkboxObject = shadowDom.getElementById(`season_${seasonNumber}_episode_${i+1}_checkbox`);
+        checkboxObject.addEventListener('change', () => {
+            data.episodeArray[seasonNumber - 1][i] =  !data.episodeArray[seasonNumber - 1][i];
+        })
+    }
+    
+}
+
+/**
+ * 
+ * @param {a reference to the show data object} data 
+ * @param {the season number for the season where the episodes are currently being diplayed 
+ * in the epsides section} seasonNumber 
+ * @returns 
+ */
+function generatedInnerHTML(data, seasonNumber){
+    let innerHTML =
+                `<div> 
+                    <h4 id="tvshowheader">TV Show</h4>
+                    <button> + </button>
+                    <h4>Add show</h4>
+                </div>
+                <img src=${data.imgSrc} alt=${data.imgAlt}>
+                <div> 
+                    <p class="title">Name: ${data.showTitle}</p>
+                    <button>Edit </button>
+                    <button>trash </button>
+                </div>
+                <p class="rating">Rating: ${data.rating}/5</p>
+                <p class="comments">Comments: ${data.comments}</p>
+                <h2 id="progressheader">Progress: </h2>` +
+                generateSeasonsHTML(data.episodeArray) + 
+                generateEpisodesForSeason(data.episodeArray[seasonNumber-1] ,seasonNumber);
+                //+ `<p class="watched">${WatchedEpisodes(data.episodeArray)}/${TotalEpisodes(data.episodeArray)} episodes watched</p>`;
+                return innerHTML;
+}
+
+/**
+ * Generates a HTML string for the episodes section
+ * @param {an array of booleans repersenting which episodes are watched in a season} episodes 
+ * @param {the current season of the show being displayed} seasonNumber
+ * @returns a HTML string used to generate the episodes section
+ */
+ function generateEpisodesForSeason(episodes, seasonNumber){
+    let s = `<div>`;
+    for(let i = 0; i < episodes.length; i++){
+        let checked;
+        episodes[i] ? checked= "checked" : checked = "";
+        s += `<input type="checkbox" id="season_${seasonNumber}_episode_${i+1}_checkbox" ${checked} class="showBox"> </input>`;
+    }
+    s += `</div>`;
+    return s;
+}
+
 
 // Example of an "episode array"
 // Season 1: we watched epsiodes 1, 2, 4. Four total episodes.
@@ -96,12 +172,13 @@
  */
 function generateSeasonsHTML(episodes){
     let s = `<div>`;
-    for(var i = 0; i < episodes.length; i++){
-        s += `<button id="season_` + (i+1) + `_button">Season ` + (i+1) + `</button>`;
+    for(let i = 0; i < episodes.length; i++){
+        s += `<button id="season_` + (i+1) + `_button" class="seasonButton">Season ` + (i+1) + `</button>`;
     }
     s += `</div>`;
     return s;
 }
+
 
 
 /** 
