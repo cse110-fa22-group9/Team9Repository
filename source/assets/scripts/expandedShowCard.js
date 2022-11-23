55e3d7c4eef1acbba1f4c55c964741c8ad951d23
Lanmei0914 <c4yan@ucsd.edu>
2022-11-22 21:07:00 -0800

    Merge With CardCSS

--- source/assets/scripts/expandedShowCard.js
+++ source/assets/scripts/expandedShowCard.js
@@ -15,11 +15,6 @@
  * 
  * data() - sets the show card data with the information provided from the data object
  */
-
-//importing functions from tools.js
-import {getShowsFromStorage} from './tools.js';
-import {saveShowsToStorage} from './tools.js';
-
  class expandedShowCard extends HTMLElement {
     /**
      * Construct a expandedShowCard element
@@ -31,7 +26,158 @@ import {saveShowsToStorage} from './tools.js';
         let article = document.createElement('article');
         let style = document.createElement('style');
 
-        //style.textContent = `` -- TODO: decide on style later
+        style.innerHTML = `
+
+        #outerbox {
+            background-color: rgb(1, 107, 112);
+            padding: 0em 2em 1em 2em;
+            overflow-x: scroll;
+            resize: none;
+            width: 76em;
+        }
+
+        #homebutton {
+            border: 0;
+            background-color: rgb(1, 107, 112);
+            float: right;
+            margin-right: 0.5em;
+        }
+
+        .toptvshowheader {
+            background-color: rgb(1, 107, 112);
+            display: flex;
+            justify-content: space-between;
+            width: 100%;
+            font-family: Arial;
+            color: white;
+            height: 3em;
+        }
+
+        .half{
+            display: flex;
+	        align-items: center;
+	        justify-content: space-between;
+        }
+
+        #tvshowheader {
+            color: white;
+            margin-left: 1em;
+        }
+
+        #innerbox {
+            background-color: rgb(17, 151, 157);
+            padding: 0.7em 0.7em 0.7em 0.7em;
+        }
+
+        #showandinfo {
+            display: flex;
+        }
+
+        #info {
+            margin-left: 0.7em;
+        }
+        
+        #titleandbuttons {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            height: 2.5em;
+            width: 47em;
+        }
+
+        #titleandbuttons p {
+            display: inline;
+            height: 2.5em;
+            line-height: 2.5em; 
+            font-family: 'Oswald', sans-serif;
+            color: white;
+            font-size: 1.75em;
+            margin: 0;
+        }
+
+        #titleandbuttons button {
+            display: inline;
+            margin-left: 0.6em;
+            border: 0;
+            background-color: rgb(17, 151, 157);
+        }
+
+        .rating {
+            height: 2.5em;
+            line-height: 2.5em; 
+            font-family: 'Oswald', sans-serif;
+            color: white;
+            font-size: 1.75em;
+        }
+
+        .comments {
+            height: 2.4em;
+            line-height: 1.6em; 
+            font-family: 'Oswald', sans-serif;
+            color: white;
+            font-size: 1.75em;
+        }
+
+        #progressheader {
+            font-family: 'Oswald', sans-serif;
+            margin-top: 0.3em;
+            margin-bottom: 0em;
+            color: white;
+            font-weight: normal;
+            font-size: 1.75em;
+        }
+
+        #seasonbuttons {
+            padding: 0;
+            height: 2.5em;
+        }
+
+        .currentSeasonButton{
+            width: 13em;
+            height: 3em;
+            background-color: rgb(5, 94, 98);
+            color: white;
+            border: none;
+            padding: 0;
+            outline: none;
+            vertical-align: text-bottom;
+        }
+
+        .seasonButton{
+            width: 6.5em;
+            height: 2.5em;
+            background-color: rgb(1, 107, 112);
+            color: white;
+            border: none;
+            padding: 0;
+            outline: none;
+            vertical-align: text-bottom;
+        }
+
+        #episodesDiv{
+            background-color: rgb(219, 253, 255);
+            padding-top: 1em;
+            padding-right: 1em;
+            padding-bottom: 3em;
+            padding-left: 1em;
+        }
+
+        .showBoxChecked{
+            background-color: rgb(12, 167, 137);
+            width: 6.5em;
+            height: 2.5em;
+            border-style: solid;
+            padding: 0;
+        }
+
+        .showBoxUnchecked{
+            background-color: white;
+            width: 6.5em;
+            height: 2.5em;
+            border-style: solid;
+            padding: 0;
+        }
+        `;
 
         shadow.append(article);
         shadow.append(style);
@@ -54,7 +200,6 @@ import {saveShowsToStorage} from './tools.js';
      *                            "episodeArray": 2D array of booleans representing show and seasons
      *                            "rating" : number
      *                            "comments" : "string"
-     *                            "id": number
      *                        }
      */
     set data(data) {
@@ -109,16 +254,18 @@ function CreateActionListeners(data, seasonNumber, shadowDom){
         });
     }
 
-    // get local storge
-    let cards = getShowsFromStorage();
-
     for(let i = 0; i < data.episodeArray[seasonNumber - 1].length; i++){
         let checkboxObject = shadowDom.getElementById(`season_${seasonNumber}_episode_${i+1}_checkbox`);
-        checkboxObject.addEventListener('change', () => {
+        checkboxObject.addEventListener('click', () => {
             data.episodeArray[seasonNumber - 1][i] =  !data.episodeArray[seasonNumber - 1][i];
-            cards[data.id] = data;
-            // update local storge
-            saveShowsToStorage(cards);
+            //toggle the display class
+            if(data.episodeArray[seasonNumber - 1][i]){ //checked
+                checkboxObject.classList.remove("showBoxUnchecked");
+                checkboxObject.classList.add("showBoxChecked");
+            }else{ //unchecked
+                checkboxObject.classList.remove("showBoxChecked");
+                checkboxObject.classList.add("showBoxUnchecked");
+            }
         })
     }
     
@@ -133,22 +280,37 @@ function CreateActionListeners(data, seasonNumber, shadowDom){
  */
 function generatedInnerHTML(data, seasonNumber){
     let innerHTML =
-                `<div> 
-                    <h4 id="tvshowheader">TV Show</h4>
-                    <button> + </button>
-                    <h4>Add show</h4>
-                </div>
-                <img src=${data.imgSrc} alt=${data.imgAlt}>
-                <div> 
-                    <p class="title">Name: ${data.showTitle}</p>
-                    <button>Edit </button>
-                    <button>trash </button>
-                </div>
-                <p class="rating">Rating: ${data.rating}/5</p>
-                <p class="comments">Comments: ${data.comments}</p>
-                <h2 id="progressheader">Progress: </h2>` +
-                generateSeasonsHTML(data.episodeArray) + 
-                generateEpisodesForSeason(data.episodeArray[seasonNumber-1] ,seasonNumber);
+                `<div id="outerbox">
+                    <div class="toptvshowheader"> 
+                        <div class="half"><h4 id="tvshowheader">TV Show</h4></div>
+                        <div class="half"><a href="../../index.html"><button id="homebutton">
+                            <img height="35em" src="../img/icons/home.png"></img>
+                        </button></a></div>
+                    </div>
+                    <div id="innerbox">
+                        <div id="showandinfo">
+                            <img id="showimage" src=${data.imgSrc} alt=${data.imgAlt} height="240 em">
+                            <div id="info">
+                                <div id="titleandbuttons"> 
+                                    <p class="title">Name: ${data.showTitle}</p>
+                                    <div id="buttons">
+                                        <button id="editbutton">
+                                            <img height="27em" src="../img/icons/edit.png"></img>
+                                        </button>
+                                        <button id="editbutton">
+                                            <img height="27em" src="../img/icons/trash.png"></img>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div class="rating">Rating: ${data.rating}/5</div>
+                                <div class="comments">Comments: ${data.comments}</div>
+                            </div>
+                        </div>
+                        <h2 id="progressheader">Progress: </h2>` +
+                        generateSeasonsHTML(data.episodeArray, seasonNumber) + 
+                        generateEpisodesForSeason(data.episodeArray[seasonNumber-1] ,seasonNumber) +
+                        `</div>
+                </div>`
                 //+ `<p class="watched">${WatchedEpisodes(data.episodeArray)}/${TotalEpisodes(data.episodeArray)} episodes watched</p>`;
                 return innerHTML;
 }
@@ -160,11 +322,11 @@ function generatedInnerHTML(data, seasonNumber){
  * @returns a HTML string used to generate the episodes section
  */
  function generateEpisodesForSeason(episodes, seasonNumber){
-    let s = `<div>`;
+    let s = `<div id="episodesDiv">`;
     for(let i = 0; i < episodes.length; i++){
         let checked;
-        episodes[i] ? checked= "checked" : checked = "";
-        s += `<input type="checkbox" id="season_${seasonNumber}_episode_${i+1}_checkbox" ${checked} class="showBox"> </input>`;
+        episodes[i] ? checked= "showBoxChecked" : checked = "showBoxUnchecked";
+        s += `<button id="season_${seasonNumber}_episode_${i+1}_checkbox" class="${checked}"> ${i+1} </button>`;
     }
     s += `</div>`;
     return s;
@@ -182,42 +344,13 @@ function generatedInnerHTML(data, seasonNumber){
  * You can click through the top labels, each labeled "Season X", to switch between progress bars
  * Each bit of progress is marked by an image.
  */
-function generateSeasonsHTML(episodes){
-    let s = `<div>`;
+function generateSeasonsHTML(episodes, seasonNumber){
+    let s = `<div id="seasonbuttons">`;
     for(let i = 0; i < episodes.length; i++){
-        s += `<button id="season_` + (i+1) + `_button" class="seasonButton">Season ` + (i+1) + `</button>`;
+        s += `<button id="season_` + (i+1) + `_button" class="${seasonNumber == (i+1) ? "currentSeasonButton" : "seasonButton"}">Season ` + (i+1) + `</button>`;
     }
     s += `</div>`;
     return s;
-}
-
-
-
-/** 
- * Returns the total number of episodes for the show, given an episode array
-*/
-function TotalEpisodes(episodes) {
-    let count = 0;
-    for(let i = 0; i < episodes.length; i++){
-        count += episodes[i].length;
-    }
-    return count;
-}
-
-/** 
- * Returns the total number of episodes for the show that have been 
- * WATCHED (marked true), given an episode array
-*/
-function WatchedEpisodes(episodes){
-    let count = 0;
-    for(let i = 0; i < episodes.length; i++){
-        for(let j = 0; j < episodes[i].length; j++){
-            if(episodes[i][j]){
-                count++;
-            }
-        }
-    }
-    return count;
 }
 
 customElements.define("expanded-show-card", expandedShowCard);
